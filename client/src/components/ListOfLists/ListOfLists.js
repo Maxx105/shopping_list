7b70eafa21320a6b698fc6b6437b626b14824721
Maxx Sanner <maxxsanner105@gmail.com>
2021-02-24 19:50:44 -0800

    added ability to cross out items

--- client/src/components/ListOfLists/ListOfLists.js
+++ client/src/components/ListOfLists/ListOfLists.js
@@ -15,8 +15,8 @@ function ListOfLists(props) {
             <ul className="list-group">
                 {props.lists.map((list) => (
                     <div className="input-group mb-1" key={list._id}>
-                        <Link to={`/lists/${list._id}`}>
-                            <li className="list-group-item">{list.name}</li>
+                        <Link to={`/lists/${list._id}`} style={{color: "black"}}>
+                            <li className="list-group-item" id="list-name">{list.name}</li>
                         </Link>
                         <div className="input-group-append">
                             <button type="submit" className="btn btn-danger" onClick={() => deleteListName(list._id)}> 
